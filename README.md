0772faa5dfb38572eab49572ef53a5f5d7df291e
gogs <you@example.com>
2019-06-08 04:42:48 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # custombuilder
+To build the builder image:
+docker build . --file Dockerfile --tag go-custom-docker-builder
+
+To run builder for test or debug (note that docker.sock needed for docker ops within the builder):
+docker run -it --privileged -v /run/docker.sock:/run/docker.sock go-custom-docker-builder bash
--- build.sh
+++ build.sh
@@ -8,38 +8,43 @@ if [ ! -e "${DOCKER_SOCKET}" ]; then
   exit 1
 fi
 
-SECRET_PATH=${PUSH_DOCKERCFG_PATH:-}
-
-if [ -z "${SECRET_PATH}" ]; then
-  echo "The dockercfg not found in /var/run/secrets/openshift.io/push"
-  exit 1
-fi
+#SECRET_PATH=${PUSH_DOCKERCFG_PATH:-}
+#
+#if [ -z "${SECRET_PATH}" ]; then
+#  echo "The dockercfg not found in /var/run/secrets/openshift.io/push"
+#  exit 1
+#fi
 
 if [ -n "${OUTPUT_IMAGE}" ]; then
   TAG="${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}"
 fi
 
-mkdir -p /tmp/build && cd /tmp/build
-cp -v $SECRET_PATH /tmp/build/dockercfg
-chmod 0666 /tmp/build/dockercfg
+#mkdir -p /tmp/build && cd /tmp/build
+#cp -v $SECRET_PATH /tmp/build/dockercfg
+#chmod 0666 /tmp/build/dockercfg
 
 # This ruby app just output content of file referenced by the environment
 # variable. For example FOO=/tmp/test and then GET /FOO returns content of
 # /tmp/test
-cat > config.ru <<- EOF
-def readfile(name); File.read(ENV[name]) rescue "not found #{ENV[name]}"; end
-run Proc.new { |env|
-  path = env['PATH_INFO'].gsub(/^\//,'')
-  [200, {"Content-Type" => "text/raw"}, [readfile(path)]]
-}
-EOF
+#cat > config.ru <<- EOF
+#def readfile(name); File.read(ENV[name]) rescue "not found #{ENV[name]}"; end
+#run Proc.new { |env|
+#  path = env['PATH_INFO'].gsub(/^\//,'')
+#  [200, {"Content-Type" => "text/raw"}, [readfile(path)]]
+#}
+#EOF
 
+# Create the package
+mkdir -p /tmp/build && cd /tmp/build
+git clone https://github.com/p-sforza/golang-ex.git
+cd golang-ex/
+go build
+
+# Create the docker file
 cat > Dockerfile <<- EOF
-FROM centos/ruby-22-centos7
-ENV SECRET_FILE /opt/openshift/src/dockercfg
-COPY dockercfg ./
-COPY config.ru ./
-CMD /usr/local/sti/run
+FROM openshift/origin-base 
+COPY golang-ex $HOME
+CMD  $HOME/golang-ex
 EOF
 
 docker build --rm -t "${TAG}" .
